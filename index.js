b47c942893fb1012af829ef5212cbf128211af50
Andy Johnson <andyrjohnson82@gmail.com>
2018-01-28 10:27:16 -0800

    fix: no more mutating actions param

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ function updateGlobalState(name, data) {
 
 module.exports = function hmr(app) {
   return function (state, actions, view, container) {
+    const newActions = {};
     var storageName = "hmrState";
     updateGlobalState(storageName, state);
     Object.keys(actions).forEach(function(key) {
       var action = actions[key];
-      actions[key] = function() {
+      newActions[key] = function() {
         var result = action.apply(this, arguments);
         updateGlobalState(storageName, actions.getState());
         return result;
       }
     });
-    actions.getState = function() { 
+    newActions.getState = function() { 
       return function(state) {
         return state;
       };
     }
-    return app(state, actions, view, container);
+    return app(state, newActions, view, container);
   }
 };

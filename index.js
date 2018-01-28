9245978b07883ab07e43a9f4955fe92a7f2a60b2
Andy Johnson <andyrjohnson82@gmail.com>
2018-01-28 10:39:09 -0800

    fix: bug with last commit, getState() call was undefined...

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function updateGlobalState(name, data) {
 
 module.exports = function hmr(app) {
   return function (state, actions, view, container) {
+    let result;
     const newActions = {};
     var storageName = "hmrState";
     updateGlobalState(storageName, state);
@@ -11,7 +12,7 @@ module.exports = function hmr(app) {
       var action = actions[key];
       newActions[key] = function() {
         var result = typeof action === "function" ? action.apply(this, arguments) : action;
-        updateGlobalState(storageName, actions.getState());
+        updateGlobalState(storageName, result.getState());
         return result;
       }
     });
@@ -20,6 +21,7 @@ module.exports = function hmr(app) {
         return state;
       };
     }
-    return app(state, newActions, view, container);
+    result = app(state, newActions, view, container);
+    return result;
   }
 };
